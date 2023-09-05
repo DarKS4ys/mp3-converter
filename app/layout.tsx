0d4a9054eac8c64ad10b5fff17c874cd8877b672
DarKS4ys <melihyardim1057@gmail.com>
2023-09-05 22:54:18 +0300

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Stoic Converter',
-  description: 'Convert your desired MP3 files from Youtube',
+  title: 'Youtube MP3 Converter',
+  description: 'Convert your desired MP3 files from Youtube & download them',
 }
 
 export default function RootLayout({
