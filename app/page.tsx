af8c01d7248d086679abaad163d8b6398908d48b
DarKS4ys <melihyardim1057@gmail.com>
2023-09-05 22:48:45 +0300

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,43 @@ import Input from "./components/Input";
 import clsx from "clsx";
 import axios from "axios";
 import { YoutubeParser } from "./utils/YtParser";
+import SocialLink from "./components/SocialLink";
+import {SiTiktok } from 'react-icons/si'
+import Image from "next/image";
+import {AiFillYoutube} from "react-icons/ai"
+import { BsTrash } from "react-icons/bs";
+import { BiLoader } from "react-icons/bi";
 
 export default function Home() {
   const inputUrlRef = useRef<HTMLInputElement | null>(null);
   const [urlResult, setUrlResult] = useState(null);
   const [inputValue, setInputValue] = useState<string>("");
+  const [videoTitle, setVideoTitle] = useState<string>("");
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const RumbleIcon = 'RumbleIcon.svg'
+
+  const isYouTubeUrlValid = (url: string): boolean => {
+    // Use a regular expression to check if the URL is a valid YouTube video URL
+    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+/;
+    return youtubeRegex.test(url);
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (inputUrlRef.current !== null) {
-      const youtubeId = YoutubeParser(inputUrlRef.current.value)
+      const inputUrl = inputUrlRef.current.value;
+      
+      if (!isYouTubeUrlValid(inputUrl)) {
+        // Display an error message to the user or handle invalid URL case
+        console.error("Invalid YouTube URL");
+        return;
+      }
+
+      const youtubeId = YoutubeParser(inputUrl)
+      setLoading(true);
       console.log(youtubeId);
 
-      const apiKey = process.env.API_KEY;
       const options = {
         method: 'GET',
         url: 'https://youtube-mp36.p.rapidapi.com/dl',
@@ -31,8 +55,15 @@ export default function Home() {
         }
       }
       axios(options)
-       .then(res => setUrlResult(res.data.link))
+       .then(res => {
+        setVideoTitle(res.data.title);
+        setUrlResult(res.data.link)
+      
+      })
        .catch(err => console.log(err))
+       .finally(() => {
+        setLoading(false);
+      })
 
        inputUrlRef.current.value = ""
     }
@@ -44,8 +75,8 @@ export default function Home() {
 
   return (
     <main className="flex flex-col justify-center items-center w-full h-screen text-center gap-4 p-8">
-      <h1 className="text-5xl font-semibold">
-        Stoic<span className="text-red-600">Converter</span>
+      <h1 className="text-5xl font-semibold text-red-600">
+        Youtube MP3 <span className="text-white">Converter</span>
       </h1>
       <section className="sm:w-[50rem] gap-4 flex flex-col">
         <p className="text-xl">
@@ -60,29 +91,50 @@ export default function Home() {
             value={inputValue}
             onChange={handleInputChange}
           />
+          {urlResult ?
+             <Button 
+             className="group font-semibold text-stone-300 hover:text-white transition !w-36"
+            onClick={() => {
+              setUrlResult(null)
+              setInputValue("")
+              setVideoTitle("")
+            }}>
+              <p className="group-hover:opacity-0 transition">Remove</p>
+             <BsTrash className="group-hover:-translate-x-8 transition group-hover:scale-125"/>
+            </Button>
+          :
           <Button
-            className={clsx("!w-36", {
-              "text-gray-500 hover:ring-0 hover:bg-transparent hover:border-[--border] hover:ring-offset-0": inputValue === "",
-            })}
-            type="submit"
-            disabled={inputValue === ""}
-          >
-            {inputValue === "" ? "Empty" : "Search"}
-          </Button>
+          className={clsx("!w-36", {
+            "cursor-not-allowed hover:bg-transparent text-[--border] hover:ring-offset-0 hover:ring-0": inputValue === "",
+          })}
+          type="submit"
+          disabled={inputValue === "" || isLoading}
+        >
+         {isLoading ? <BiLoader className="animate-spin"/> : inputValue === "" ? "Empty" : "Search"}
+        </Button>
+        }
         </form>
 
-        {
-        
+        {    
         urlResult ?
-        
         <a target='_blank' rel="norefrrer" href={urlResult}>
-          <Button className="bg-red-600 hover:bg-red-500 ring-red-500/50">
+          <Button className="bg-red-600 hover:bg-red-500 !ring-red-500/60">
             Download MP3
           </Button>
         </a> : null 
-        
         }
-
+        {videoTitle ?
+        <p>{videoTitle}</p>
+        :null}
+      <hr className="border-[--border]"/>
+        <div className="flex justify-center items-center gap-4">
+          <SocialLink iconSize={36} link="/tiktok" icon={SiTiktok}/>
+          <SocialLink link="/rumble">
+            <Image  width={36} height={36} src={RumbleIcon} alt="Rumble Icon"/>
+          </SocialLink>
+          <SocialLink link="/ytmp3" iconSize={36} icon={AiFillYoutube}>
+          </SocialLink>
+        </div>
 
       </section>
     </main>
